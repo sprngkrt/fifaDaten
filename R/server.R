e3f0116b179a2f873f28edd819639992f58a05fd
springkart <nicolai.springkart@gmail.com>
2018-08-26 17:40:43 +0200

    Grundgerüst steht

--- R/server.R
+++ R/server.R
@@ -3,6 +3,7 @@ rm(list =ls())
 
 
 library(shiny)
+
 library(dplyr)
 library(countrycode)
 
@@ -16,7 +17,6 @@ rawData <- rawData %>%
 
 #couCon <- codelist %>%
 #  select(country.name.en, continent)
-
 
 
 home <- rawData
@@ -34,27 +34,24 @@ data <- union(home, away)
 #  select(country.name.en, continent)
 
 
+teams <- data %>%
+  distinct(Mannschaft)
+
 
 function(input, output, session) {
 
   # Combine the selected variables into a new data frame
-  selectedData <- reactive({
-    iris[, c(input$xcol, input$ycol)]
-  })
+  Spiele <- reactive({
+    data %>%
+     filter(Mannschaft == input$man1,
+            Gegner == input$man2) %>%
+    select (Tore, Gegentore)
 
-  clusters <- reactive({
-    kmeans(selectedData(), input$clusters)
   })
 
   output$plot1 <- renderPlot({
-    palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3",
-              "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999"))
-
-    par(mar = c(5.1, 4.1, 0, 1))
-    plot(selectedData(),
-         col = clusters()$cluster,
-         pch = 20, cex = 3)
-    points(clusters()$centers, pch = 4, cex = 4, lwd = 4)
+    plot(Spiele())
   })
 
 }
+
--- R/ui.R
+++ R/ui.R
@@ -4,13 +4,10 @@
 pageWithSidebar(
   headerPanel('Iris k-means clustering'),
   sidebarPanel(
-    selectInput('xcol', 'X Variable', names(iris)),
-    selectInput('ycol', 'Y Variable', names(iris),
-                selected=names(iris)[[2]]),
-    numericInput('clusters', 'Cluster count', 3,
-                 min = 1, max = 9)
-  ),
+    selectInput("man1", "Mannschaft 1: ", teams, selected = "Germany"),
+    selectInput("man2", "Mannschaft 2: ", teams, selected = "England") ),
+
   mainPanel(
-    plotOutput('plot1')
+    plotOutput("plot1")
   )
 )
